03336c695c818fb866f3a024cb327067066f6329
VeluguPrathyusha <163579200+VeluguPrathyusha@users.noreply.github.com>
2024-04-28 23:00:08 +0530

    Add files via upload

---
+++ lung.py
@@ -0,0 +1,64 @@
+### Health Management APP
+from dotenv import load_dotenv
+
+load_dotenv() ## load all the environment variables
+
+import streamlit as st
+import os
+import google.generativeai as genai
+from PIL import Image
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+## Function to load Google Gemini Pro Vision API And get response
+
+def get_gemini_repsonse(input,image,prompt):
+    model=genai.GenerativeModel('gemini-pro-vision')
+    response=model.generate_content([input,image[0],prompt])
+    return response.text
+
+def input_image_setup(uploaded_file):
+    # Check if a file has been uploaded
+    if uploaded_file is not None:
+        # Read the file into bytes
+        bytes_data = uploaded_file.getvalue()
+
+        image_parts = [
+            {
+                "mime_type": uploaded_file.type,  # Get the mime type of the uploaded file
+                "data": bytes_data
+            }
+        ]
+        return image_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
+    
+##initialize our streamlit app
+
+st.set_page_config(page_title="brain tumor ")
+
+st.header(" lung dection ")
+input=st.text_input("Input Prompt: ",key="input")
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+image=""   
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
+
+
+submit=st.button(" Predict ")
+
+input_prompt="""
+
+Given an input image of a chest X-ray, analyze the image to determine whether it shows signs of pneumonia, tuberculosis, both, or neither. Look for common patterns associated with each disease, such as opacities for pneumonia and cavitation for tuberculosis. Consider patient history or symptoms if provided. Output the diagnosis as one of the following: 'Pneumonia,' 'Tuberculosis,' 'Both,' or 'Neither,' along with a confidence level for each classification if possible."
+
+output  = is  normal or  Pneumonia
+"""
+
+## If submit button is clicked
+
+if submit:
+    image_data=input_image_setup(uploaded_file)
+    response=get_gemini_repsonse(input_prompt,image_data,input)
+    st.subheader("The Response is")
+    st.write(response)
